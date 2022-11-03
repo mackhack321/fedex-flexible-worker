e8fd83dfcf79a62a3e7b3aba369cf14d8538c58a
Mack Stanley <mlgprohacker@gmail.com>
2022-11-03 11:40:52 -0500

    colors :D

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,17 @@
 module.exports = {
   content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        "fedex-purple": "#4D168A",
+        "fedex-orange": "#FD601B",
+        "fedex-red": "#DC0033",
+        "fedex-blue": "#007AB7",
+        "fedex-placeholder": "#333333",
+        "fedex-grey": "#F2F2F2",
+        "fedex-header-black": "#1E1E1E",
+      },
+    },
   },
   plugins: [],
 };
