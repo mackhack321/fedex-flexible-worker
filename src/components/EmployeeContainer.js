8de34f2061d2886940f72a3999528f112c258385
Mack Stanley <mlgprohacker@gmail.com>
2022-11-06 12:11:08 -0600

    set up employee container and navbar

--- src/components/EmployeeContainer.js
+++ src/components/EmployeeContainer.js
@@ -1,5 +1,17 @@
 import React from "react";
+import Navbar from "./Navbar";
+import bill from "../resources/bill.jpg";
+import { Outlet } from "react-router-dom";
 
 export default function EmployeeContainer() {
-  return <div>EmployeeContainer</div>;
+  return (
+    <div>
+      <Navbar name="Bill" pfp={bill} homepage="employee" />
+      <main className="grid place-items-center">
+        <div className="w-full px-10 lg:w-2/3 lg:px-0 3xl:w-1/3">
+          <Outlet />
+        </div>
+      </main>
+    </div>
+  );
 }
