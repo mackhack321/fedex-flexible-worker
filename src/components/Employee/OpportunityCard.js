206ae564447060735c9dc588aa454d8a5136c1fd
Mack Stanley <mlgprohacker@gmail.com>
2022-11-06 12:34:45 -0600

    add claim button to op card

--- src/components/Employee/OpportunityCard.js
+++ src/components/Employee/OpportunityCard.js
@@ -4,7 +4,17 @@ import { Link } from "react-router-dom";
 export default function OpportunityCard(props) {
   return (
     <div className="w-fit rounded-md bg-fedex-grey p-5">
-      <div className="mb-2 text-3xl font-light">{props.opportunity.title}</div>
+      <div className="flex justify-between">
+        <div className="mb-2 text-3xl font-light">
+          {props.opportunity.title}
+        </div>
+        <Link
+          to={`opportunity/${props.opportunity.id}/claim`}
+          className="h-fit rounded-md border-2 border-fedex-orange bg-fedex-orange px-2 py-1 text-center font-bold text-white"
+        >
+          CLAIM
+        </Link>
+      </div>
       <div className="mb-4 flex flex-col space-y-2 md:flex-row md:space-y-0">
         <div className="md:w-60">{props.opportunity.description}</div>
         <div className="flex flex-col lg:mr-16">
